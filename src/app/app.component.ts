628322df21ba2bdc3d29fbd352a6f6ff3c65bef8
vidprod108 <vidprod108@gmail.com>
2021-02-12 14:51:23 -0500

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component, VERSION } from '@angular/core';
 
 @Component({
-  selector: 'my-app',
+  selector: 'my-first-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
